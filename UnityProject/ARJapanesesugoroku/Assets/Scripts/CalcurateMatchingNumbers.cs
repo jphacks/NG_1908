f3a0579c00634f6546b55df9926f56e05da51d0f
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-10-19 12:17:40 +0900

    WaitSceneの作成,機能の追加

---
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/CalcurateMatchingNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class CalcurateMatchingNumbers : MonoBehaviour
+{
+    public Text playernumbertext;
+    private int playernumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playernumber = PhotonNetwork.PlayerList.Length;
+        playernumbertext.text = "マッチング人数 " + playernumber + "人";
+    }
+}
