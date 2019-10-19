c0866c5a5d861d9dcf5f86b3eec4c500acd3c4d1
kotao2700 <sketdonce@gmail.com>
2019-10-19 14:32:41 +0900

    UI追加

---
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/CourseDecide/StartCountdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartCountdown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject countdowner;
+    public GameObject Goalbutton;
+    public Text text;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GoalButtonclicked()
+    {
+        countdowner.gameObject.SetActive(true);
+        Goalbutton.gameObject.SetActive(false);
+        text.text = "スタート位置についてね";
+
+    }
+}
