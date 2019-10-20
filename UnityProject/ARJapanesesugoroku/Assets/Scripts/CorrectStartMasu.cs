03e71b27a779d2f6866bbb09ad57d7a7d9674d23
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-10-20 10:13:11 +0900

    CorrectStartMasuの追加,Gamemanagerの変更

---
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/CorrectStartMasu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectStartMasu : MonoBehaviour
+{
+    //監視用のフラグ
+    [SerializeField]
+    bool Ready = false;
+    //Update動かす用のフラグ
+    bool flag = false;
+    public void Update()
+    {
+        if (flag == true)
+        {
+            
+        }
+    }
+    public void WaitingAllPlayers()
+    {
+        flag = true;
+    }
+}
