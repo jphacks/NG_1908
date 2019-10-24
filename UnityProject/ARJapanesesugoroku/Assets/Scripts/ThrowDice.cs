b2e15e51f189e8f3b87542b69ac93b697d9390c8
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-10-24 18:30:16 +0900

    他の人がダイスを触れないようにした

--- UnityProject/ARJapanesesugoroku/Assets/Scripts/ThrowDice.cs
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/ThrowDice.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ThrowDice : MonoBehaviour
 {
+    private PhotonView m_photonView;
     public Rigidbody rb;
     void Start()
     {
+        m_photonView = GetComponent<PhotonView>();
         rb = GetComponent<Rigidbody>();
     }
     public void PushUp()
     {
+        if (!m_photonView.IsMine)
+        {
+            return;
+        }
         GetComponent<Rigidbody>().useGravity = true;
         rb.AddForce( Camera.main.transform.rotation * new Vector3(0,150,1000));
     }
