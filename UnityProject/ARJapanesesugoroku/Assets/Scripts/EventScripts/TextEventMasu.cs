a32dd8d4ccf6600f956faec081dad46b7c6b1683
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-12-05 19:13:12 +0900

    メタファイル上げ忘れた

---
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/EventScripts/TextEventMasu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextEventMasu : EventMasu
+{
+    public override void RaiseEvent()
+    {
+        EndProcess();
+    }
+    public virtual void TextEvent()
+    {
+
+    }
+}
