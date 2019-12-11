eb6483fc1092414a2be65c96335fe91caadc49fa
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-12-11 16:30:29 +0900

    UItext表示用のスクリプトを作成、エディタ拡張に伴い必要なEditorファイルを作成

---
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/TextMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class TextMaster 
+{
+    //stateとMainプレイヤー、それ以外のプレイヤーに表示するテキスト
+    public string state="";
+    public string mainText="";
+    public string othersText="";
+
+    public TextMaster(string state, string mainText, string othersText)
+    {
+        state = this.state;
+        mainText = this.mainText;
+        othersText = this.othersText;
+    }
+
+}
---
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/TextMasterController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMasterController : MonoBehaviour
+{
+    //ここにUIに使うテキストリストを表示させる。
+    public List<TextMaster> list = new List<TextMaster>();
+
+}
