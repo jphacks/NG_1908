cdf6ff437c64afa049a24ec3aad28bd725d1c219
kotao2700 <sketdonce@gmail.com>
2019-12-14 16:10:57 +0900

    タイトル画面エラー修正

--- UnityProject/ARJapanesesugoroku/Assets/Scripts/TitleManaging.cs
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/TitleManaging.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TitleManaging : MonoBehaviour
 {
-    public GameObject button1;
     public GameObject button2, clowd1, clowd2,txt;
     // Start is called before the first frame update
     void Start()
