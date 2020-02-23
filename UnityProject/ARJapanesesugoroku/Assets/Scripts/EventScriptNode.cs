e0499c9ee90351115d2c7150512fd25de6831e38
shoi <shoi.itou@gmail.com>
2020-02-23 11:08:09 +0900

    イベントマス作成画面の作成

---
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/EventScriptNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventScriptNode : MonoBehaviour
+{
+    [SerializeField] private InputField inputField;
+    
+    //テスト用
+    private string _eventText;
+    
+    public void SetEventText()
+    {
+        //本来はEventScriptsクラスにInputFieldの値を代入
+
+        _eventText = inputField.text;
+    }
+
+    public void DeleteNode()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ UnityProject/ARJapanesesugoroku/Assets/Scripts/EventScriptScrollView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventScriptScrollView : MonoBehaviour
+{
+    [SerializeField] private GameObject eventScriptNode;
+    [SerializeField] private GameObject content;
+    [SerializeField] private InputField eventSetNameInputField;
+    
+
+    public void AddEventScriptNode()
+    {
+        Instantiate(eventScriptNode, content.transform);
+    }
+
+    public void SaveEventScriptNodes()
+    {
+        //EventScriptのリストを取得
+//        var nodes = content.GetComponentsInChildren<EventScripts>();
+
+        var eventSetName = eventSetNameInputField.text;
+
+        //ここで保存関数を呼ぶ
+        Debug.Log("保存! " + eventSetName);
+    }
+}
